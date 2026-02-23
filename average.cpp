9863c68fe58cd68ceb674791b3a53a4935683a39
irxzleam <maelzxri@gmail.com>
2026-02-23 18:16:17 +0700

    avg

--- average.cpp
+++ average.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+#include<string>
 
+using namespace std;
+
+int main(int argc, char* argv[]){
+    if(argc==1){
+        cout << "Please input numbers to find average.";
+        return 0;
+    }
+    double sum = 0;
+    int n = argc-1;
+    for(int i = 1; i<argc; i++){
+        sum += stod(argv[i]);
+    }
+    double avg = sum/n;
+    cout << "---------------------------------\n";
+    cout << "Average of " << n << " numbers = " << avg << "\n";
+    cout << "---------------------------------";
+}
